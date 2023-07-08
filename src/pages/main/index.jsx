757677846286307b27061dbd93c97f9bd3045e9f
Mehmet Aydin <110032695+Mehmet1414@users.noreply.github.com>
2023-07-08 19:46:45 +0200

    slider tamamlandi

--- src/pages/main/index.jsx
+++ src/pages/main/index.jsx
@@ -4,6 +4,66 @@ const MainPage = () => {
   return (
     <>
       <Slider />
+
+      {/* <!-- mobile showcase --> */}
+      <div className="space-medium">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+              <div className="showcase-block">
+                <div className="display-logo ">
+                  <a href="#">
+                    <img src="../public/assets/images/nexus.png" alt="" />
+                  </a>
+                </div>
+                <div className="showcase-img">
+                  <a href="#">
+                    <img
+                      src="../public/assets/images/display_img_1.png"
+                      alt=""
+                    />
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+              <div className="showcase-block">
+                <div className="display-logo ">
+                  <a href="#">
+                    <img src="../public/assets/images/iphone.png" alt="" />
+                  </a>
+                </div>
+                <div className="showcase-img">
+                  <a href="#">
+                    <img
+                      src="../public/assets/images/display_img_2.png"
+                      alt=""
+                    />
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+              <div className="showcase-block">
+                <div className="display-logo ">
+                  <a href="#">
+                    <img src="../public/assets/images/samsung.png" alt="" />
+                  </a>
+                </div>
+                <div className="showcase-img">
+                  <a href="#">
+                    <img
+                      src="../public/assets/images/display_img_3.png"
+                      alt=""
+                    />
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* <!-- /.mobile showcase --> */}
     </>
   );
 };
